aefe6e43451e10907ab494a5957176c09da71c5e
Eugene <me.eugene.south@gmail.com>
2026-05-16 23:10:57 +0300

    router mainLayout

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,19 @@
-// FILE: vite.config.js
-
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/CODD/' : '/',
   plugins: [
     vue({
       template: { transformAssetUrls }
     }),
 
-    // @quasar/plugin-vite options list:
-    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
       sassVariables: fileURLToPath(
         new URL('./src/quasar-variables.sass', import.meta.url)
       )
     })
-  ]
+  ],
 })
